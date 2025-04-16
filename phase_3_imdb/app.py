2d0afdcf28df53842deeb7d67c65bf0ad26bd222
Ani Nele <123322304+aninele@users.noreply.github.com>
2025-04-16 15:56:00 -0400

    Update app.py

--- phase_3_imdb/app.py
+++ phase_3_imdb/app.py
@@ -189,5 +189,5 @@ if __name__ == "__main__":
     # Only call init_app() in dev/test, not production deploy
     if os.getenv("FLASK_ENV") != "production":
 
-    app.run(host="0.0.0.0", port=8080, debug=False)
+        app.run(host="0.0.0.0", port=8080, debug=False)
 
